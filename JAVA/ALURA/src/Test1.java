84c8205b713e51b2f9f5104fa700b5d1648876e3
Nicolas <ncmartinez018@gmail.com>
2023-02-16 10:17:04 -0500

    Inicio curso de programación back end

---
+++ JAVA/ALURA/src/Test1.java
@@ -0,0 +1,20 @@
+public class Test1 {
+    public static void main(String[] args) throws Exception {
+        
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 65;                    // Compila!
+        System.out.println(valor);
+
+        valor = (char) (valor + 1);         // Compila!
+        System.out.println(valor);
+
+        String palabra = "Alura cursos online de tecnología";
+        System.out.println(palabra);
+
+        palabra = palabra + 2020;
+        System.out.println(palabra);
+        
+    }
+}
