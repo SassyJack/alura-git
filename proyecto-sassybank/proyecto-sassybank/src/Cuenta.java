92156206f756ad48a4943a0d3ff1bc848ed41159
Nicolas <ncmartinez018@gmail.com>
2023-02-28 13:34:01 -0500

    Metod transferir y cuenta pipe

--- proyecto-sassybank/proyecto-sassybank/src/Cuenta.java
+++ proyecto-sassybank/proyecto-sassybank/src/Cuenta.java
@@ -6,16 +6,28 @@ public class Cuenta {
 
      public void depositar(double valor){
           //El this esta es una referencia, es decir, "apunta" a un objeto.
-          this.saldo = this.saldo + valor;
+          this.saldo += valor;
      }
      public boolean retirar(double valor){
                if (valor <= this.saldo) {
-                    this.saldo = this.saldo - valor;
+                    this.saldo -= valor;
                     return true;
                }
                else{
                     System.out.println("No hay fondos suficientes");
                     return false;
                }
+          }
+     public boolean transferir(double valor, Cuenta cuenta) {
+          if (valor <= this.saldo){
+               this.saldo -= valor;
+               cuenta.depositar(valor);
+               return true;
+          }
+          else{
+               System.out.println("No hay fondos suficientes");
+                    return false;
+          }
      }
+     
 }
--- proyecto-sassybank/proyecto-sassybank/src/PruebaMetodos.java
+++ proyecto-sassybank/proyecto-sassybank/src/PruebaMetodos.java
@@ -1,11 +1,15 @@
 public class PruebaMetodos {
     public static void main(String[] args) {
         Cuenta cuentaRamon = new Cuenta();
-
+        Cuenta cuentaPipe = new Cuenta();
         cuentaRamon.saldo = 500;
         cuentaRamon.depositar(300);
         System.out.println(cuentaRamon.saldo);
         cuentaRamon.retirar(1000);
+        System.out.println(cuentaRamon.saldo);
+        cuentaPipe.depositar(10000);
+        cuentaPipe.transferir(200,cuentaRamon);
+        System.out.println(cuentaPipe.saldo);
         System.out.println(cuentaRamon.saldo);
     }
 }
