8782b92a3b1514d7f522374e18a1158105b28943
Nicolas <ncmartinez018@gmail.com>
2023-03-07 10:37:27 -0500

    Nuevo Proyecto de herencia y polimorfismo

---
+++ ProyectoBank - Herencias y polimorfismo/src/Funcionario.java
@@ -0,0 +1,27 @@
+public class Funcionario {
+    private String nombre;
+    private int documento;
+    private double salario;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getDocumento() {
+        return documento;
+    }
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+
+}
---
+++ ProyectoBank - Herencias y polimorfismo/src/TestFuncionario.java
@@ -0,0 +1,10 @@
+public class TestFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario1 = new Funcionario();
+
+        funcionario1.setDocumento(1000455897);
+        funcionario1.setNombre("Ramon");
+        funcionario1.setSalario(3000000);
+        
+    }
+}
