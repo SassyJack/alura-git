8a4c85fe2a911209963c5b1eb1ad77c80960f89c
Nicolas <ncmartinez018@gmail.com>
2023-03-23 12:49:04 -0500

    Modificacion para uso propio

--- br.com.alura/br.com.alura/src/Alumno.java
+++ br.com.alura/br.com.alura/src/Alumno.java
@@ -1,4 +1,4 @@
-package com.alura.model;
+
 
 public class Alumno {
 
--- br.com.alura/br.com.alura/src/Clase14.java
+++ br.com.alura/br.com.alura/src/Clase14.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 
 public class Clase14 {
     public static void main(String[] args) {
-        Alumno alumno1 = new Alumno("Ramon", 05);
-        Alumno alumno2 = new Alumno("Pipe", 01);
-        Alumno alumno3 = new Alumno("Kaka", 02);
-        Alumno alumno4 = new Alumno("Checho", 03);
+        Alumno alumno1 = new Alumno("Ramon", "05");
+        Alumno alumno2 = new Alumno("Pipe", "01");
+        Alumno alumno3 = new Alumno("Kaka", "02");
+        Alumno alumno4 = new Alumno("Checho", "03");
     
         Collection<Alumno> listaAlumnos = new ArrayList<>();
 
@@ -16,7 +15,7 @@ public class Clase14 {
         listaAlumnos.add(alumno3);
         listaAlumnos.add(alumno4);
 
-        Alumno alumnoNuevo = new Alumno("Ramon", 05);
+        Alumno alumnoNuevo = new Alumno("Ramon", "05");
 
         System.out.println(alumno1.equals(alumnoNuevo));
         System.out.println(listaAlumnos.contains(alumnoNuevo));
