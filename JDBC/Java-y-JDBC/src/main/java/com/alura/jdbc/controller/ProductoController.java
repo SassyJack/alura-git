367065675ae3dc8831c1ea2c548112990e76a82d
Nicolas <ncmartinez018@gmail.com>
2023-05-11 13:01:16 -0500

    Conexion de una aplicacion java con una dataBase

---
+++ JDBC/Java-y-JDBC/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -0,0 +1,36 @@
+package com.alura.jdbc.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoController {
+
+	public void modificar(String nombre, String descripcion, Integer id) {
+		// TODO
+	}
+
+	public void eliminar(Integer id) {
+		// TODO
+	}
+
+	public List<?> listar() throws SQLException {
+		Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+                "root",
+                "SassyJ*1806");
+                Statement statement = con.createStatement();
+                boolean result = statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+                System.out.println(result);
+                con.close();
+		return new ArrayList<>();
+	}
+
+    public void guardar(Object producto) {
+		// TODO
+	}
+
+}
